631484365631f356724feed67cf02ed1b43e73f9
Masaga8 <masaga8@gmail.com>
2018-03-12 18:51:28 +0100

    something

--- script_basic.py
+++ script_basic.py
@@ -6,18 +6,36 @@ from sys import argv
 
 p = PDBParser(PERMISSIVE = 1)
 
+# Create pdb objects. Save them in a dictionary. 
 allpdb = {}
-chains = {}
+chains = set()
+
 for filename in argv:
 	if argv.index(filename) == 0:
 		continue
 	else:
 		allpdb[filename] = p.get_structure(filename, filename)
+		for chain in allpdb[filename].get_chains():
+			chains.add(chain)
+		#print(set((allpdb[filename].get_chains())).difference(chains))
+
 		
+		#chains.update(set((allpdb[filename].get_chains())).difference(chains))
+		#print(chains)
+		#chains.update(list(allpdb[filename].get_chains()))
+
+# Obtain a dictionary with a list list of interactions (keys are chains)
+#interactions = {}
+#chains = []
+#	for pdb in allpdb:
+#		for chain in list(element.get_chains()):
+#			interacions[chains] = append.chains 
 
 
 
-for element in allpdb:
-	for a in element:
-		print(element["A"])
+#for element in allpdb:
+#	for a in element:
+#		print(element["A"])
 	#print(list(element.get_chains()))
+
+print(chains)
