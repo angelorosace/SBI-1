8e949e5c686b2cd316dc9f75669de45a19b6b1e3
comradeameba <d.a.g@hotmail.com.ar>
2018-03-13 17:59:58 +0100

    superimposer began. First loop optimized with dict comprehension

--- script_basic.py
+++ script_basic.py
@@ -4,93 +4,61 @@ from Bio.PDB import *
 from sys import argv
 import re
 
+def superimpose(fix_chain,mov_chain):
+	"""
+	Superimpose peptide chain mov_chain on fix_chain with Biopython superimposer.
+	"""
+	sup = Superimposer()
+
+	#//Obtenir una llista d'atoms-objects per a cada cadena
+	fix_atoms = list(fix_chain.get_atoms())
+	mov_atoms = list(mov_chain.get_atoms())
+	#//Fer superimposicio
+	sup.set_atoms(fix_atoms	,mov_atoms)
+
+	#//Matriu amb Rotacio/translacio/rmsd a aplicar a la estructura
+	print(sup.rotran)
+	print(sup.rms)
+
+	#//Aplicar rotacio/translacio als atoms mov. Per aplicar la mateixa rotacio a altres cadenes es podria fer servir la matriu anterior
+	sup.apply(mov_atoms)
+
 # Create a PDBparser object
 parser = PDBParser(PERMISSIVE = 1)
 
 # Create a structure object. All structure objects are in a dictonary 
 # We also obtain a set with the different chains (subunits of the macromolecula) 
 # Moreover, we create a dictionary that contains a list with the id subunits interactiong by filename (key)
-allpdb = {}
+
+#Create a structure object for every file and store them as values in a dictionary with name of file as key
+allpdb = { filename : parser.get_structure(filename, filename) for filename in argv if argv.index(filename) != 0}
+
+#Store all chain id in a set. Create a dictionary where every filename has a list of chain-objects as values
 subunits = set()
 chainsbyfile = {}
-chainslist = list()
+for pdb in allpdb:
+	chainsbyfile[pdb] = list()
+	for subunit in allpdb[pdb].get_chains():
+		chainsbyfile[pdb].append(subunit)
+		subunits.add(subunit.get_id())
 
-for filename in argv:
-	if argv.index(filename) == 0:
-		continue
-	else:
-		allpdb[filename] = parser.get_structure(filename, filename)
-		subuid_last = str()
-		for subunit in allpdb[filename].get_chains():
-			suid = subunit.get_id()
-			chainslist.append(suid)
-			subunits.add(suid)
-		chainsbyfile[filename] = chainslist
-		chainslist = list()
-
-			
-# Create a dictionary with all the subunits as keys and a list with the subunits with which interacts
+# Create a dictionary with all the subunits as keys and, as values, a list with the subunits which interacts with
+###########NOTA D'OPIMITZACIO: Aixo es podria acoblar al loop anterior######################### 
 interactions = {}
 intlist = list()
 
 for element in subunits: 
-	for filename in argv:
-		if argv.index(filename) == 0:
-			continue
-		else:
-			if element in chainsbyfile[filename]:
-				interact = chainsbyfile[filename] 
-				index = interact.index(element)
-				if index == 0:
-					intlist.append(interact[1])
-					
-				else:
-					intlist.append(interact[0])
+	for filename in allpdb:
+		if element in chainsbyfile[filename]:
+			interact = chainsbyfile[filename] 
+			index = interact.index(element)
+			if index == 0:
+				intlist.append(interact[1])
+				
+			else:
+				intlist.append(interact[0])
 	interactions[element] = intlist
 	intlist = list()
 
-
-
-		
-
-
-		
-# Obtain a dictionary with a list of interactions (keys are subunits -chains in the case of simple complex-)
-#interactions = {}
-#chains = []
-#for element in subunits:
-#	for pdb in allpdb:
-#		for chain in :
-#			interacions[chains] = append.chains 
-
-
-#for element in allpdb:
-#	for a in element:
-#		print(element["A"])
-	#print(list(element.get_chains()))
-
-#my_string =  "<Chain id=C>"
-#p = re.compile("<Chain id=(.*)>")
-#m = p.match(my_string)
-#hey = m.groups()
-#print(hey[0])
-
-#print(chains)
-#chains = set()
-#for structure in allpdb:
-#	#print(structure)
-#	for model in allpdb[structure]:
-#		#print(model)
-#		for chain in model:
-#			chains.add(chain.get_id())
-			#print(dir(chain))
-			#print(help(id))
-			#m = p.match(chain)
-			#chainid = m.groups()
-			#chains.add(chainid[0])
-
-
-#interactions[subuid] = intlist.append(subuid_last)
-#			interactions[subuid_last] = intlist.append(subuid)
-#			subuid_last = subuid
-#		subuid_last = str()
+#
+superimpose(chainsbyfile["1fn3_AB.pdb"][0], chainsbyfile['1fn3_AD.pdb'][0])
