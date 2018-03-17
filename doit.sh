a018771fca5f95d533976c10b9ce897bd8121c7b
comradeameba <d.a.g@hotmail.com.ar>
2018-03-17 14:10:41 +0100

    clash control

--- doit.sh
+++ doit.sh
@@ -1,2 +1,2 @@
-python3 script_basic.py AB.pdb AC.pdb AD.pdb BC.pdb DC.pdb
+python3 script_basic.py AB.pdb AD.pdb BC.pdb DC.pdb
 
--- script_basic.py
+++ script_basic.py
@@ -1,11 +1,50 @@
 #!/usr/bin/python3
 
 from Bio.PDB import *
-from sys import argv, exit
+from sys import *
 import os
 
 io = PDBIO()
 os.system("rm temp*.pdb")
+
+def savepdb (pdb_ob,filename):
+	"""
+	Save an object in pdb format
+	"""
+	io.set_structure(pdb_ob)
+	io.save(filename)
+
+def clashtest(chain1, chain2):
+	"""
+	Returns True if any of the atoms on chain1 clash with any of the atoms in chain2
+	"""
+	clashcounter = 0
+
+	#Obtain a list of atoms for chain1 and a list of coordinates for chain 2
+	atom1 = [ atom for atom in chain1.get_atoms() ]
+	cordsatom2 = [ atom.get_coord() for atom in chain2.get_atoms() ]
+
+	#Obtain number of atoms in both chains
+	numatoms1 = len(atom1)
+	numatoms2 = len(cordsatom2)
+
+	#Prepare list of atoms for neighbour search
+	ns = NeighborSearch(atom1)
+
+	#Check collisions of every atom in chain2 to already-prepared-list of atoms1 (collision distance: 1 amstrong)
+	for clashes in map(lambda x: ns.search(x,2.0), cordsatom2):
+		#Count number of atoms clashing
+		if len(clashes)> 0:
+			clashcounter += 1
+
+	#If any atom clashes, calculate the overall percentage of clashing atoms in chain1
+	#//El missatge d'error el deixo de moment com a demostracio. A la versio definitiva no hi hauria de ser
+	clashpercent  = clashcounter/numatoms1 * 100
+	if clashcounter > 0:
+		stderr.write("%i per cent atoms in chain %s are clashing with chain %s\n" %(clashpercent, chain1.get_id(), chain2.get_id()))
+
+	#Return percentage of clashing atoms
+	return clashpercent
 
 def superimpose(fix_chain,mov_chain,apply_chain):
 	"""
@@ -22,25 +61,23 @@ def superimpose(fix_chain,mov_chain,apply_chain):
 	sup.apply(apply_chain)
 	return apply_chain
 
-count = 0
-def recursive(chain_ob, camefrom, interactions, allpdb, count):
+def recursive(chain_ob, camefrom, interactions, allpdb):
 	"""
 	NOTA A MARTA: Aquesta subrutina, benvolguda Marta, potser ens solucioni gran part del treball o potser ens peti l'ordinador. L'important es que sortirem de dubtes
 	L'he comentat molt perque la puguis entendre facil i rapidament
 	ALERTA!!!!: si visualitzes els resultats a chimera es veuran malament la majoria dels cops, doncs aquest algoritme te tendencia a repetir subunitats. I si dues subunitats estan molt a prop, chimera les separa
 	"""
-	count += 1
 	chainame = chain_ob.get_id()
 	#//Tancament de seguretat per evitar que se li vagi l'olla
-	if count == 5:
+	if counter == 5:
 		exit()
-
 	#Iterate throught the chains which interact with the present chain
 	for chainteracting in interactions[chainame]:
 		#If this loop corresponds to the already superimposed chain where the present chain_ob comes from, skip it
 		if chainteracting == camefrom:
 			continue
 
+		#print("chain fixed: %s\nchain to move: %s" % (chain_ob,chainteracting))
 		#Extract the pdb where chain_ob and chainteracting are both toghether
 		pdbname = interactions[chainame][chainteracting]
 		pdb_ob = allpdb[pdbname]
@@ -48,26 +85,39 @@ def recursive(chain_ob, camefrom, interactions, allpdb, count):
 		chaintomove = pdb_ob[0][chainame]
 		#aplichain: chain that will be moved to interact with chain_ob
 		applychain = pdb_ob[0][chainteracting]
+
+		#Check collistions
+		clashtest(chain_ob, applychain)
+
 		#Superimpose
 		appliedchain = superimpose(fix_chain = chain_ob, mov_chain = chaintomove, apply_chain = applychain)
 
+		#Check if appliedchain collides with any of the 
+		for chainmoved in chainsmoved[appliedchain.get_id()]:
+			clashperc = clashtest(appliedchain, chainmoved)
+			if clashperc > 80:
+				return 
+		#print("chain %s moved succesfully\n" % (appliedchain))
+		#Add applied chain to coresponding list of already-moved chains
+		chainsmoved[appliedchain.get_id()].append(appliedchain)
+		#Add one to the superimpositions counter
+		global counter
+		counter += 1
+
 		#Save appliedchain (the moved one) in a new pdb
-		io.set_structure(appliedchain)
-		tempname = "temp" + str(count) + ".pdb"
-		io.save(tempname)
+		tempname = "temp" + str(counter) + ".pdb"
+		savepdb(appliedchain, tempname)
 
 		#Repeat process for appliedchain
-		recursive(appliedchain, chainame, interactions, allpdb, count)
+		recursive(appliedchain, chainame, interactions, allpdb)
 
-# Create a PDBparser object
-parser = PDBParser(PERMISSIVE = 1)
 
 ##########################################
 ##Store Relations between files and chains
 ##########################################
 
 """
-Dictionary and list index:
+Dictionary, list and other variables index:
 	1. Allpdb:
 		keys: input pdb filenames
 		values: Structure pdb object from file
@@ -79,13 +129,21 @@ Dictionary and list index:
 	4. Interactions: 
 		key: chain name
 		value: list of chain names wich interacts with
-
+	5. counter: counts the number of chains moved for bulding the present model
+	6. Chainsmoved: dictionary with
+		keys: all chains in the present model
+		values: chain_objects moved of the corresponding chain type
 """
 
-#Create a structure object for every file and store them as values in a dictionary with name of file as key
+# Create a PDBparser object
+parser = PDBParser(PERMISSIVE = 1)
+
+#declare some variables
+
+#Create allpdb dictionary
 allpdb = { filename : parser.get_structure(filename, filename) for filename in argv if argv.index(filename) != 0}
 
-#Store all chain id in a set. Create a dictionary where every filename has a list of chain-objects as values
+#Create subunits set and chainsbyfile dictionary
 subunits = set()
 chainsbyfile = {}
 for pdb in allpdb:
@@ -94,11 +152,7 @@ for pdb in allpdb:
 		chainsbyfile[pdb].append(subunit)
 		subunits.add(subunit.get_id())
 
-# Create a dictionary with all the subunits as keys and, as values, a list with the subunits which interacts with
-# The list is a list of dictionaries, becaise every chain id has a reference to the pdb were was found
-###########NOTA A DAVID: Hi ha d'haver una forma millor de fer aixo######################### 
-######## MARTA: He afegit d'on venen les chains (és a dir de quin pdb surten). Sinó era info que no et portava enlloc
-######## DAVID: Em sembla una bona idea, pero ho he canviat a diccionaris perque sia mes optim. Fer una llista de diccionaris d'una entrada es molt absurd
+#Create intersections dictionary
 interactions = {}
 intset = dict()
 
@@ -115,10 +169,18 @@ for element in subunits:
 	interactions[element] = intset
 	intset = dict()
 
+#Counter of chains
+global counter
+counter = 0
+
+#Create chainsmoved dictionary
+chainsmoved = { chain: [] for chain in subunits }
+
 #print("allpdb: ",allpdb)
 #print("subunits: ",subunits)
 #print("chainsbyfile: ",chainsbyfile)
 #print("interactions: ",interactions)
+#print("chainsmoved: ",chainsmoved)
 
 """
 //NOTA A SANDVITX: He canviat els pdbs que utiltzem com a probes pels que fan servir Oriol i Alvaro. Perque? perque no hi havia caigut en que, malgrat estiguin en fitxers separats, les nostres
@@ -126,16 +188,6 @@ cadenes ja estan positionades on els toca, d'ons venen d'un fitxer comu
 Ara surten un munt de warnings, pero no t'alarmis, funciona igualment. Es perque els pdbs que utilitzen Alvaro i Oriol els falta una columna 
 """
 
-#//Sobre com fer el pdb output. No he trobat manera humana de fer-ho d'una forma mes neta. Si en trobes una millor, fes'la, pero no t'hi matis buscant. Ja he perdut jo prou temps com perque t'hi matis tu ara
-#//MARTA al habla: Ho deixo de mostra per no carregar-m'ho al intentar fer les iteracions per arribar a la macromolècula. 
-#io.set_structure(chainsbyfile["AB.pdb"][0])
-#io.save("temp1.pdb")
-#io.set_structure(chainsbyfile["AB.pdb"][1])
-#io.save("temp2.pdb")
-#moved_chain = superimpose(chainsbyfile["AB.pdb"][0], chainsbyfile['AD.pdb'][0], chainsbyfile['AD.pdb'][1])
-#io.set_structure(moved_chain)
-#io.save("temp3.pdb")
-#os.system("cat temp*.pdb > krosis.pdb")
 
 """
 # For each pdb we use the first chain as the fixed chain (reference) and save the second one
@@ -182,11 +234,24 @@ for option in [[0,1],[1,0]]:
 """
 
 
-#//Camp de proves
 
-#Obtain a random pdb interactino file
+#Obtain a random pdb interacting file
 seed = list(allpdb.keys())[0]
-
 #Iterate over pdb seed file chains
+areclashes = False
 for chain in chainsbyfile[seed]:
-	recursive(chain, chainsbyfile[seed][1].get_id(), interactions, allpdb, count)
+
+	#Check collisions for seed chains
+	for chainmoved in chainsmoved[chain.get_id()]:
+		clashperc = clashtest(chain, chainmoved)
+		if clashperc > 80:
+			areclashes = True 
+
+	#save seed chains 
+	if not areclashes:
+		savepdb(chain, "temp" + str(counter) + ".pdb")
+		chainsmoved[chain.get_id()].append(chain)
+
+		recursive(chain, chainsbyfile[seed][1].get_id(), interactions, allpdb)
+
+os.system("cat temp*.pdb > krosis.pdb")
